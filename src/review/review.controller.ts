87f48f28b3ea9c04e56ecd485ea487d6733583e3
holfizz <gorlach7v@gmial.com>
2023-10-06 19:10:42 +0300

    test

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common'
+import { ReviewService } from './review.service'
 
 @Controller('review')
 export class ReviewController {
-	// constructor(private readonly reviewService: ReviewService) {}
+	constructor(private readonly reviewService: ReviewService) {}
 }
